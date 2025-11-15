c98184ba6d51f54c3b051c27fc3cd5ac2b7907a9
SamVP29 <samuel.v3r1@gmail.com>
2025-11-15 18:20:38 -0500

    Deploy portafolio

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = "samvp29.github.io";
-
 export default defineConfig({
-    plugins: [react()],
-    base: process.env.VITE_BASE_PATH ?? `/${repoName}/`,
-    server: {
-        port: 5173,
-        host: "0.0.0.0",
-    },
+plugins: [react()],
+    base: "/", 
 });
